0d992ee08bc3440503e4c4f69b7e95b36d5df0ee
masajiro <49312834+Masajiro999@users.noreply.github.com>
2022-12-13 11:51:00 +0000

    env設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   runtimeConfig: {
-    openaiApiKey: process.env.OPENAI_API_KEY,
+    openaiApiKey: process.env.OPEN_AI_KEY,
   },
 })
